91d36738ed0b79d396bbdbc2af538e09fef72ece
ComanderP <8596680+ComanderP@users.noreply.github.com>
2023-11-06 01:38:28 +0100

    Bun?

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,119 @@ fn main()
 {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.5, 0.5, 0.9)))
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_systems(Startup, setup)
-        .add_systems(Update, change_clear_color)
+        .add_systems(Update, (animate_sprite, move_bun))
         .run();
 }
 
-fn setup(mut commands: Commands)
+#[derive(Component)]
+struct AnimationIndices
 {
-    commands.spawn(Camera2dBundle::default());
+    first: usize,
+    last: usize,
 }
 
-fn change_clear_color(input: Res<Input<KeyCode>>, mut clear_color: ResMut<ClearColor>)
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+)
 {
-    if input.just_pressed(KeyCode::Space)
+    let texture_handle = asset_server.load("bun.png");
+    let texture_atlas =
+        TextureAtlas::from_grid(texture_handle, Vec2::new(48.0, 48.0), 4, 4, None, None);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    // Use only the subset of sprites in the sheet that make up the run animation
+    let animation_indices = AnimationIndices { first: 1, last: 6 };
+
+    // Spawn a camera
+    commands.spawn(Camera2dBundle::default());
+    // Spawn a sprite with the default texture atlas sprite
+    commands.spawn((
+        SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            sprite: TextureAtlasSprite::new(animation_indices.first),
+            transform: Transform::from_scale(Vec3::splat(6.0)),
+            ..default()
+        },
+        // Animation data
+        animation_indices,
+        AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+        // Very bad way of moving the sprite lmfao
+        Direction::None,
+    ));
+}
+
+fn animate_sprite(
+    time: Res<Time>,
+    mut query: Query<(
+        &AnimationIndices,
+        &mut AnimationTimer,
+        &mut TextureAtlasSprite,
+    )>,
+)
+{
+    for (indices, mut timer, mut sprite) in &mut query
     {
-        clear_color.0 = Color::PURPLE;
+        // SPRITESHEET GOES BRRRRR
+        timer.tick(time.delta());
+        if timer.just_finished()
+        {
+            sprite.index = if sprite.index == indices.last
+            {
+                indices.first
+            }
+            else
+            {
+                sprite.index + 1
+            };
+        }
+    }
+}
+
+#[derive(Component)]
+enum Direction
+{
+    None,
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+// Very bad
+fn move_bun(keyboard_input: Res<Input<KeyCode>>, mut query: Query<(&mut Direction, &mut Transform)>)
+{
+    for (mut direction, mut transform) in &mut query
+    {
+        *direction = Direction::None;
+        if keyboard_input.pressed(KeyCode::Up)
+        {
+            *direction = Direction::Up;
+        }
+        if keyboard_input.pressed(KeyCode::Down)
+        {
+            *direction = Direction::Down;
+        }
+        if keyboard_input.pressed(KeyCode::Left)
+        {
+            *direction = Direction::Left;
+        }
+        if keyboard_input.pressed(KeyCode::Right)
+        {
+            *direction = Direction::Right;
+        }
+        match *direction
+        {
+            Direction::Up => transform.translation.y += 1.0,
+            Direction::Down => transform.translation.y -= 1.0,
+            Direction::Left => transform.translation.x -= 1.0,
+            Direction::Right => transform.translation.x += 1.0,
+            _ => (),
+        }
     }
 }
