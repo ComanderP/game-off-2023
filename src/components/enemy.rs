a84d30b37d0e617db63efb3e42b38ca3948d6263
Mihai Spinei <spineim22@gmail.com>
2023-11-09 20:22:37 +0100

    adding the sprite to the feesh man

--- src/components/enemy.rs
+++ src/components/enemy.rs
@@ -17,7 +17,18 @@ pub struct Enemy;
 #[derive(Component)]
 pub struct MeleeRange(pub f32);
 
-pub fn spawn_enemy(mut commands: Commands, asset_server: Res<AssetServer>) {
+pub fn spawn_enemy(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let texture_handle = asset_server.load("feesh_man_sheet.png");
+
+    let texture_atlas = 
+        TextureAtlas::from_grid(texture_handle, Vec2::new(32.0, 32.0), 4, 1, None, None);
+    
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    
     commands.spawn((
         Enemy,
         Health {
@@ -29,8 +40,9 @@ pub fn spawn_enemy(mut commands: Commands, asset_server: Res<AssetServer>) {
             size: Vec2::new(16., 30.),
         },
         MeleeRange(20.0),
-        SpriteBundle {
-            texture: asset_server.load("man_transp.png"),
+        SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            sprite: TextureAtlasSprite::new(0),
             transform: Transform::from_xyz(200., 200., 0.),
             ..Default::default()
         },
