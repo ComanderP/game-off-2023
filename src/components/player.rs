abdf2de134fb7d09bd6f4e1e1bbc3b567cfe3dda
ComanderP <8596680+ComanderP@users.noreply.github.com>
2023-11-18 00:13:47 +0100

    Add Attacking

--- src/components/player.rs
+++ src/components/player.rs
@@ -10,12 +10,15 @@ use bevy_sprite3d::Sprite3dParams;
 pub struct PlayerPlugin;
 
 #[derive(Resource)]
-pub struct PlayerSettings {
+pub struct PlayerSettings
+{
     camera_locked: bool,
 }
 
-impl Plugin for PlayerPlugin {
-    fn build(&self, app: &mut App) {
+impl Plugin for PlayerPlugin
+{
+    fn build(&self, app: &mut App)
+    {
         app.insert_resource(PlayerSettings {
             camera_locked: true,
         });
@@ -23,7 +26,7 @@ impl Plugin for PlayerPlugin {
         app.add_systems(Update, update_player.run_if(in_state(GameState::Ready)));
         app.add_systems(
             Update,
-            update_player_sprite.run_if(in_state(GameState::Ready)),
+            (update_player_sprite, swipe_attack).run_if(in_state(GameState::Ready)),
         );
         //app.add_systems(Startup, spawn_player)
         //    .add_systems(Update, (update_player, level_up));
@@ -39,7 +42,8 @@ pub struct AnimationTimer(Timer);
 pub struct AttackTimer(Timer);
 
 #[derive(Component)]
-pub enum AnimationState {
+pub enum AnimationState
+{
     Idle,
     Moving,
     Attacking,
@@ -52,7 +56,8 @@ pub fn spawn_player(
     mut commands: Commands,
     assets: Res<MyAssets>,
     mut sprite_params: Sprite3dParams,
-) {
+)
+{
     commands.spawn((
         Player,
         Health {
@@ -64,6 +69,7 @@ pub fn spawn_player(
         AttackTimer(Timer::from_seconds(0.5, TimerMode::Repeating)),
         Xp(0),
         Speed(3.5),
+        Damage(10),
         Unit {
             size: Vec2::new(0.5, 0.5),
         },
@@ -101,50 +107,66 @@ pub fn update_player(
     colliders: Query<(&Transform, &Collider), (Without<Unit>, Without<Camera>)>,
     input: Res<Input<KeyCode>>,
     time: Res<Time>,
-) {
+)
+{
     let dtime = time.delta_seconds();
-    for (mut transform, _, speed, unit, mut state, attack_timer) in &mut players {
+    for (mut transform, _, speed, unit, mut state, attack_timer) in &mut players
+    {
         let mut direction = Vec3::ZERO;
-        if input.pressed(KeyCode::W) {
+        if input.pressed(KeyCode::W)
+        {
             direction.z -= 1.;
             // transform.rotate(Quat::from_rotation_y(2. * std::f32::consts::PI * dtime));
         }
-        if input.pressed(KeyCode::S) {
+        if input.pressed(KeyCode::S)
+        {
             direction.z += 1.;
         }
-        if input.pressed(KeyCode::D) {
+        if input.pressed(KeyCode::D)
+        {
             direction.x += 1.;
             transform.rotation = Quat::from_xyzw(0., 1., 0., 0.);
         }
-        if input.pressed(KeyCode::A) {
+        if input.pressed(KeyCode::A)
+        {
             direction.x -= 1.;
             transform.rotation = Quat::from_xyzw(0., 0., 0., 1.);
         }
-        if input.just_pressed(KeyCode::Y) {
+        if input.just_pressed(KeyCode::Y)
+        {
             settings.camera_locked = !settings.camera_locked;
         }
 
         // detect changes in X-axis movement
-        if matches!(*state, AnimationState::Attacking) {
+        if matches!(*state, AnimationState::Attacking)
+        {
             continue;
         }
-        if direction == Vec3::ZERO {
+        if direction == Vec3::ZERO
+        {
             *state = AnimationState::Idle;
-        } else {
+        }
+        else
+        {
             *state = AnimationState::Moving;
         }
         let direction = direction.normalize_or_zero();
 
-        if input.just_pressed(KeyCode::E) && attack_timer.0.elapsed_secs() == 0.0 {
+        if input.just_pressed(KeyCode::E) && attack_timer.0.elapsed_secs() == 0.0
+        {
             info!("Test");
             *state = AnimationState::Attacking;
             // do attacking
-        } else {
+        }
+        else
+        {
             unit.move_and_slide(&mut transform, direction, speed, &colliders, dtime);
         }
         // move camera on top of player
-        if settings.camera_locked || input.pressed(KeyCode::Space) {
-            for (_, mut camera_transform) in &mut camera {
+        if settings.camera_locked || input.pressed(KeyCode::Space)
+        {
+            for (_, mut camera_transform) in &mut camera
+            {
                 camera_transform.translation = transform.translation + CAMERA_OFFSET;
             }
         }
@@ -160,25 +182,35 @@ pub fn update_player_sprite(
         &mut AttackTimer,
     )>,
     time: Res<Time>,
-) {
-    for (_, mut state, mut atlas, mut timer, mut attack_timer) in &mut players {
+)
+{
+    for (_, mut state, mut atlas, mut timer, mut attack_timer) in &mut players
+    {
         timer.tick(time.delta());
-        if !timer.just_finished() {
+        if !timer.just_finished()
+        {
             continue;
         }
-        match *state {
+        match *state
+        {
             AnimationState::Idle => atlas.index = (atlas.index + 1) % 2,
-            AnimationState::Moving => {
+            AnimationState::Moving =>
+            {
                 atlas.index = (atlas.index + 1) % 4 + 4;
             }
-            AnimationState::Attacking => {
-                if atlas.index == 2 {
+            AnimationState::Attacking =>
+            {
+                if atlas.index == 2
+                {
                     atlas.index = 3;
-                } else {
+                }
+                else
+                {
                     atlas.index = 2;
                 }
                 attack_timer.0.tick(time.delta());
-                if timer.just_finished() {
+                if timer.just_finished()
+                {
                     *state = AnimationState::Idle;
                     attack_timer.0.reset();
                 }
@@ -190,13 +222,64 @@ pub fn update_player_sprite(
 fn level_up(
     // operate on anything that has Xp and Health
     mut query: Query<(&mut Xp, &mut Health, &mut Speed)>,
-) {
-    for (mut xp, mut health, mut speed) in query.iter_mut() {
-        if xp.0 >= 1000 {
+)
+{
+    for (mut xp, mut health, mut speed) in query.iter_mut()
+    {
+        if xp.0 >= 1000
+        {
             xp.0 -= 1000;
             health.max += 25;
             health.current = health.max;
             speed.0 += 10.0;
         }
     }
+}
+
+fn swipe_attack(
+    player: Query<(&Player, &Transform, &Damage)>,
+    mut enemies: Query<(&Enemy, &mut Health, &mut Transform), Without<Player>>,
+    input: Res<Input<KeyCode>>,
+)
+{
+    let player = player.single();
+    if input.just_pressed(KeyCode::Space)
+    {
+        for (_, mut enemy_health, mut enemy_transform) in enemies.iter_mut()
+        {
+            let player_transform = player.1;
+
+            let player_direction = player_transform.rotation.mul_vec3(Vec3::new(-1., 0., 0.));
+            info!("Player direction: {}", player_direction);
+
+            let distance_to_enemy = enemy_transform.translation - player_transform.translation;
+            info!("Distance to enemey: {}", distance_to_enemy);
+
+            let angle = player_direction.dot(distance_to_enemy.normalize_or_zero());
+            info!("Angle: {}", angle);
+
+            if angle > 0.5 && distance_to_enemy.length() < 3.0
+            {
+                let player_damage = player.2 .0;
+                info!("Damage!");
+                if enemy_health.current < player_damage
+                {
+                    info!("Enemy dead!");
+                    enemy_health.current = 0;
+                }
+                else
+                {
+                    enemy_health.current -= player_damage;
+                }
+                knockback_enemy(&mut enemy_transform, player_transform);
+            }
+        }
+    }
+}
+
+fn knockback_enemy(enemy_transform: &mut Transform, player_transform: &Transform)
+{
+    let direction = enemy_transform.translation - player_transform.translation;
+    let direction = direction.normalize_or_zero();
+    enemy_transform.translation += direction
 }
